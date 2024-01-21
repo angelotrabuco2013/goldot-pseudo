90eaddba3846c665831d7a185404763e80028875
Mariocraft987 <154646419+Mariocraft987@users.noreply.github.com>
2024-01-21 15:07:08 -0700

    Add files via upload

--- makey.js
+++ makey.js
@@ -17,12 +17,12 @@ function togglePW() {
 
 function createTopic() {
   var content;
-  content = '<form id="create" action="#" method="get"><label for="Instructions">Instructions:</label><br><textarea id="Instructions" name="Instructions" rows="5" cols="40" placeholder="Add some Instructions for your new project."></textarea><br/><textarea id="code" name="code" rows="7" cols="40" placeholder="Where the code goes."></textarea><br><label for="type">Language:</label><select name="type"><option value="html">Html</option><option value="js">Javascript</option><option value="java">Java</option><option value="python">Python</option><option value="cpp">C#</option><option value="cpp">C++</option><option value="php">PHP</option><option value="ruby">Ruby</option><option value="swift">Swift</option></select><br><br><input class="button" type="submit" value="Create"></form>';
+  content = '<a style="color: dodgerBlue;" href="make.html">Back</a><br/><br/><form id="create" action="#" method="get"><label for="Instructions">Instructions:</label><br><textarea id="Instructions" name="Instructions" rows="5" cols="40" placeholder="Add some Instructions for your new project."></textarea><br/><textarea id="code" name="code" rows="7" cols="40" placeholder="Where the code goes."></textarea><br><label for="type">Language:</label><select name="type"><option value="html">Html</option><option value="js">Javascript</option><option value="java">Java</option><option value="python">Python</option><option value="cpp">C#</option><option value="cpp">C++</option><option value="php">PHP</option><option value="ruby">Ruby</option><option value="swift">Swift</option></select><br><br><input class="button" type="submit" value="Create"></form>';
   document.getElementById("content").innerHTML = content;
 }
 
 function createProject() {
   var content;
-  content = '<p>uh oh! Looks like Goldot recieved an error!</p>';
+  content = '<h2>Uh oh! Looks like Goldot recieved an error!</h2><img src="images/Sad.png" alt="error"><br/><br/><a style="color: dodgerBlue;" href="make.html">Back</a>';
   document.getElementById("content").innerHTML = content;
 }
