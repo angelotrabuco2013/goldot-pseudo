b7d792c3810a70e77f3e61b9e35ed60b3a163624
Mariocraft987 <154646419+Mariocraft987@users.noreply.github.com>
2024-01-21 13:51:55 -0700

    A BIG update

--- makey.js
+++ makey.js
@@ -14,3 +14,15 @@ function togglePW() {
       x.type = "password";
     }
 }
+
+function createTopic() {
+  var content;
+  content = '<form id="create" action="#" method="get"><label for="Instructions">Instructions:</label><br><textarea id="Instructions" name="Instructions" rows="5" cols="40" placeholder="Add some Instructions for your new project."></textarea><br/><textarea id="code" name="code" rows="7" cols="40" placeholder="Where the code goes."></textarea><br><label for="type">Language:</label><select name="type"><option value="html">Html</option><option value="js">Javascript</option><option value="java">Java</option><option value="python">Python</option><option value="cpp">C#</option><option value="cpp">C++</option><option value="php">PHP</option><option value="ruby">Ruby</option><option value="swift">Swift</option></select><br><br><input class="button" type="submit" value="Create"></form>';
+  document.getElementById("content").innerHTML = content;
+}
+
+function createProject() {
+  var content;
+  content = '<p>uh oh! Looks like Goldot recieved an error!</p>';
+  document.getElementById("content").innerHTML = content;
+}
